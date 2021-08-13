5630a1c997f9cf4db991dbbf23ca4881a7a38354
Marcommproject <jokarzy27@gmail.com>
2021-08-13 12:40:26 -0500

    model_bundling

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/app.js',
+  mode: 'production',
+  output: {
+	path: `${__dirname}/dist`,
+	filename: 'bundle.js',
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+	rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
+	  {
+		test: /\.css$/,
+		use: [
+		  MiniCssExtractPlugin.loader,
+		  'css-loader',
+		],
+	  },
+     
+	  {
+		test: /\.(svg|gif|png|eot|woff|ttf)$/,
+		use: [
+		  'url-loader',
+		],
+	  },
+	],
+  },
+  optimization: {
+	minimize: true,
+	minimizer: [
+	  new TerserPlugin({
+		extractComments: false,
+	  }),
+	  new OptimizeCssAssetsPlugin(),
+	],
+  },
+};
